2725ae8053d402879a16c861d3ea750e04988146
kunal kumar <kunal.kumar@mymoneymantra.com>
2025-01-26 22:00:58 +0530

    w

--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const FooterMenu = () => (
-  <div style={{position: "fixed", bottom: "0", display: "flex", justifyContent: "space-around", backgroundColor: "#0056b3", color: "#fff", padding: "10px 0" }}>
+  <div style={{position: "fixed", bottom: "0", display: "flex", justifyContent: "space-around", backgroundColor: "#0056b3", color: "#fff", padding: "10px 0", width: "100%" }}>
     <p>Pay</p>
     <p>Save</p>
     <p>Invest</p>
